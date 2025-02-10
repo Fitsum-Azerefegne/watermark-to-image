03dfb0fe3989fb2f8c60d46cc51e1d30a60c06dd
Fitsum-Azerefegne <mininifinds@gmail.com>
2025-02-10 12:54:44 +0300

    Initial commit

---
+++ text-to-morse/main.py
@@ -0,0 +1,28 @@
+#morse code dictionary
+morse_code_dict = {
+    'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.',
+    'F': '..-.', 'G': '--.', 'H': '....', 'I': '..', 'J': '.---',
+    'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---',
+    'P': '.--.', 'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-',
+    'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-', 'Y': '-.--',
+    'Z': '--..', '0': '-----', '1': '.----', '2': '..---',
+    '3': '...--', '4': '....-', '5': '.....', '6': '-....',
+    '7': '--...', '8': '---..', '9': '----.'
+}
+
+# function to convert text to Morse code
+def text_to_morse(text):
+    morse_code = []
+    for char in text.upper():
+        if char in morse_code_dict:
+            morse_code.append(morse_code_dict[char])
+    return ''.join(morse_code)
+
+# Function to take input and convert to Morse code
+def convert_input_to_morse():
+    user_input = input("Enter text to convert to Morse code: ")
+    morse_output = text_to_morse(user_input)
+    print("Morse code:", morse_output)
+
+# Call the function
+convert_input_to_morse()
